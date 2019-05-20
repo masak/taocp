6cf7aa80734185e6db4a94a794f2da367e37a8a0
Carl Mäsak <cmasak@gmail.com>
2019-05-20 22:37:55 +0200

    Write another test
    It's failing.

--- test/reverse-list.ts
+++ test/reverse-list.ts
@@ -1,6 +1,34 @@
 import test from 'ava';
-import { Λ, reverseLinkedList } from '../invert-linked-list';
+import { LinkedList, Λ, reverseLinkedList } from '../invert-linked-list';
 
 test('reversing an empty list is an empty list', t => {
     t.deepEqual(reverseLinkedList({ first: Λ }), { first: Λ });
+});
+
+test('reversing a short list gives the expected result', t => {
+    let input: LinkedList<number> = {
+        first: {
+            info: 1,
+            link: {
+                info: 2,
+                link: {
+                    info: 3,
+                    link: Λ,
+                },
+            },
+        },
+    };
+    let expectedOutput: LinkedList<number> = {
+        first: {
+            info: 3,
+            link: {
+                info: 2,
+                link: {
+                    info: 1,
+                    link: Λ,
+                },
+            },
+        },
+    };
+    t.deepEqual(reverseLinkedList(input), expectedOutput);
 });
