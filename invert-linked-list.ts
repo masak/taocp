7c93deb5d5d68a647205369bdc35b70624a2d8e6
Carl Mäsak <cmasak@gmail.com>
2019-05-20 15:33:29 +0200

    Add example linked list and empty function

--- invert-linked-list.ts
+++ invert-linked-list.ts
@@ -8,3 +8,36 @@ interface ListNode<T> {
 }
 
 enum NullLink { NULL }
+const Λ = NullLink.NULL;
+
+let linkedList: LinkedList<number> = {
+    first: {
+        info: 1,
+        link: {
+            info: 2,
+            link: {
+                info: 3,
+                link: {
+                    info: 4,
+                    link: {
+                        info: 5,
+                        link: Λ,
+                    },
+                },
+            },
+        },
+    },
+};
+
+// Reverses a linked list _in place_; that is, returns the same
+// list object, but with its elements reversed. The `first` link
+// now points to the element that used to be _last_, and all the
+// other links between the nodes have been inverted.
+//
+// The example linked list above represents the list (1, 2, 3, 4, 5).
+// Reversing this list would turn it into (5, 4, 3, 2, 1).
+function reverseLinkedList<T>(list: LinkedList<T>): LinkedList<T> {
+    // TODO: write unit tests
+    // TODO: implement
+    return list;
+}
