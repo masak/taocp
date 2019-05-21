d3b9fa3e7b19c7cffc8efb0845fc0299b0371f30
claes-magnus <cmbernson@gmail.com>
2019-05-21 21:35:49 +0200

    Begin refactor. Needs linting :) (and lots of refactoring)

--- reverse-linked-list.ts
+++ reverse-linked-list.ts
@@ -1,4 +1,6 @@
-import { LinkedList, makeLinkedList } from './linked-list';
+import {
+    LinkedList
+} from './linked-list';
 
 // Reverses a linked list _in place_; that is, returns the same
 // list object, but with its elements reversed. The `first` link
@@ -7,23 +9,42 @@ import { LinkedList, makeLinkedList } from './linked-list';
 //
 // The example linked list above represents the list (1, 2, 3, 4, 5).
 // Reversing this list would turn it into (5, 4, 3, 2, 1).
-export function reverseLinkedList<T>(list: LinkedList<T>): LinkedList<T> {
+export function reverseLinkedList < T > (list: LinkedList < T > ): LinkedList < T > {
+	if (list.first === 0) return list;
 
-  function loopLinkedListObject(obj, values = []) {
-    if(obj.first === 0) {
-        return values;
-    } else if(obj.first) {
-       return loopLinkedListObject(obj.first, values)
-    } else if(obj.link === 0) {
-       values.push(obj.info);
-       return values.reverse();
-    } else {
-      values.push(obj.info); 
-      return loopLinkedListObject(obj.link, values);
+    function loopLinkedListObject(obj, values = []) {
+        if (obj.first === 0) {
+            return values;
+        } else if (obj.first) {
+            return loopLinkedListObject(obj.first, values)
+        } else if (obj.link === 0) {
+            values.unshift(obj.info);
+
+            return values.reduceRight((a, b, i) => {
+                if (i !== 0) {
+                    return {
+                        info: b,
+                        link: a
+                    };
+                } else {
+                    const newObj = {
+                        info: b,
+                        link: a
+                    };
+                    return {
+                        first: {
+                            info: values[0],
+                            ...newObj
+                        }
+                    }
+                }
+            }, 0);
+
+        } else {
+            values.unshift(obj.info);
+            return loopLinkedListObject(obj.link, values);
+        }
     }
-  } 
-  const linkedListValues = loopLinkedListObject(list);
-
-  return makeLinkedList(...linkedListValues);
+    return loopLinkedListObject(list);
 }
 
