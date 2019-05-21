1006d8d34d61eadfb318d9e53692ef228104f90e
Carl Mäsak <cmasak@gmail.com>
2019-05-21 22:38:54 +0200

    Implement iterative solution to ch2.2.3 ex7

--- reverse-linked-list.ts
+++ reverse-linked-list.ts
@@ -1,4 +1,4 @@
-import { LinkedList } from "./linked-list";
+import { LinkedList, ListNode, NullLink, Λ } from "./linked-list";
 
 // Reverses a linked list _in place_; that is, returns the same
 // list object, but with its elements reversed. The `first` link
@@ -8,39 +8,17 @@ import { LinkedList } from "./linked-list";
 // The example linked list above represents the list (1, 2, 3, 4, 5).
 // Reversing this list would turn it into (5, 4, 3, 2, 1).
 export function reverseLinkedList<T>(list: LinkedList<T>): LinkedList<T> {
-    if (list.first === 0) return list;
-
-    function loopLinkedListObject(obj, values = []) {
-        if (obj.first === 0) {
-            return values;
-        } else if (obj.first) {
-            return loopLinkedListObject(obj.first, values);
-        } else if (obj.link === 0) {
-            values.unshift(obj.info);
-
-            return values.reduceRight((a, b, i) => {
-                if (i !== 0) {
-                    return {
-                        info: b,
-                        link: a,
-                    };
-                } else {
-                    const newObj = {
-                        info: b,
-                        link: a,
-                    };
-                    return {
-                        first: {
-                            info: values[0],
-                            ...newObj,
-                        },
-                    };
-                }
-            }, 0);
-        } else {
-            values.unshift(obj.info);
-            return loopLinkedListObject(obj.link, values);
-        }
+    let previousNode: ListNode<T> | NullLink = Λ;
+    let nextNode: ListNode<T> | NullLink;
+    for (
+        let node: ListNode<T> | NullLink = list.first;
+        node !== Λ;
+        node = nextNode
+    ) {
+        nextNode = node.link;
+        node.link = previousNode;
+        previousNode = node;
     }
-    return loopLinkedListObject(list);
+    list.first = previousNode;
+    return list;
 }
