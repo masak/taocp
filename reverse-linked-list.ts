90953927828355c9b2b31fa542c7e686730a0e94
claes-magnus <cmbernson@gmail.com>
2019-05-21 17:49:44 +0200

    Add first solution. Needs to be refactored

--- reverse-linked-list.ts
+++ reverse-linked-list.ts
@@ -1,4 +1,4 @@
-import { LinkedList } from './linked-list';
+import { LinkedList, makeLinkedList } from './linked-list';
 
 // Reverses a linked list _in place_; that is, returns the same
 // list object, but with its elements reversed. The `first` link
@@ -8,6 +8,22 @@ import { LinkedList } from './linked-list';
 // The example linked list above represents the list (1, 2, 3, 4, 5).
 // Reversing this list would turn it into (5, 4, 3, 2, 1).
 export function reverseLinkedList<T>(list: LinkedList<T>): LinkedList<T> {
-    // TODO: implement
-    return list;
+
+  function loopLinkedListObject(obj, values = []) {
+    if(obj.first === 0) {
+        return values;
+    } else if(obj.first) {
+       return loopLinkedListObject(obj.first, values)
+    } else if(obj.link === 0) {
+       values.push(obj.info);
+       return values.reverse();
+    } else {
+      values.push(obj.info); 
+      return loopLinkedListObject(obj.link, values);
+    }
+  } 
+  const linkedListValues = loopLinkedListObject(list);
+
+  return makeLinkedList(...linkedListValues);
 }
+
