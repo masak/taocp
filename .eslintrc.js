8b718470769d87c63b15a7fa49a05d58f197b58e
Carl Mäsak <cmasak@gmail.com>
2019-05-21 16:20:12 +0200

    Add an .eslintrc.js configuration file
    It was ignored, and so didn't make it in the last commit.

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+  "extends": ["plugin:@typescript-eslint/recommended"],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "project": "./tsconfig.json",
+        "sourceType": "module"
+    },
+    "plugins": ["@typescript-eslint"],
+    "rules": {
+    }
+};
