e17af9a4226698a247ccafbe4f278e8761a69049
Carl Mäsak <cmasak@gmail.com>
2019-07-18 22:28:48 +0200

    Add some explanatory comments

--- ch2.2.1-ex2/permutation-to-operations.ts
+++ ch2.2.1-ex2/permutation-to-operations.ts
@@ -48,6 +48,13 @@ export function permToOps(inputPermutation: number[]): Operation[] {
     let stack = [];
     let stackingReason = new Map<number, number>();
     for (let number of inputPermutation) {
+        // CASE I(a): The number we're to output is the next one from the input.
+        //            In this case we go through the below loop once, and we're
+        //            guaranteed success afterwards. No net change to the stack.
+        // CASE I(b): The number we're to output is higher than the next one
+        //            from the input. In this case we need to stack all the
+        //            numbers we're not ready to output yet. They'll be stacked
+        //            so they need to be output in reverse (LIFO) order later.
         while (number >= nextCarFromInput) {
             outputOperations.push(Operation.S);
             stack.push(nextCarFromInput);
@@ -55,6 +62,11 @@ export function permToOps(inputPermutation: number[]): Operation[] {
             nextCarFromInput++;
         }
         let stackTop = stack[stack.length - 1];
+        // CASE II(a): The number we're to output is somewhere in the stack, but
+        //             not on top. This in the only thing we can't do, and we
+        //             need to throw an exception rather than continue. This
+        //             happens when cars were stacked in I(b) earlier, and now
+        //             they're requested in some non-LIFO order.
         if (number !== stackTop) {
             throw new CannotOutputInOriginalOrder(
                 number,
@@ -62,6 +74,11 @@ export function permToOps(inputPermutation: number[]): Operation[] {
                 stackingReason.get(number) as number,
             );
         }
+        // CASE II(b): The number we're to output is on the top of the stack.
+        //             Either because we're coming directly from case I(a), or
+        //             because things were stacked in a previous iterations's
+        //             step I(b), and they're now being requested in a LIFO
+        //             order.
         outputOperations.push(Operation.X);
         stack.pop();
     }
