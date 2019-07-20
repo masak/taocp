8c98e6a88b2923afdb4e17af9b11c1bf7ccad88e
Carl Mäsak <cmasak@gmail.com>
2019-07-20 21:14:50 +0200

    Remove unused variable

--- ch2.2.1-ex3/admissible.ts
+++ ch2.2.1-ex3/admissible.ts
@@ -1,14 +1,12 @@
 import { Operation } from "../ch2.2.1-ex2/operations";
 
 export function sequenceIsAdmissible(sequence: Operation[]): boolean {
-    let nextCarNumber = 1;
     let stackSize = 0;
 
     for (let op of sequence) {
         switch (op) {
             case Operation.S:
                 stackSize += 1;
-                nextCarNumber++;
                 break;
             case Operation.X:
                 stackSize -= 1;
