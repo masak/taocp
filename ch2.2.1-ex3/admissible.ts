525337b70dbd6b564f3511ffcb18e3d643c6aef6
Carl Mäsak <cmasak@gmail.com>
2019-07-20 21:19:17 +0200

    Change `switch` into ternary operator

--- ch2.2.1-ex3/admissible.ts
+++ ch2.2.1-ex3/admissible.ts
@@ -4,14 +4,7 @@ export function sequenceIsAdmissible(sequence: Operation[]): boolean {
     let stackSize = 0;
 
     for (let op of sequence) {
-        switch (op) {
-            case Operation.S:
-                stackSize += 1;
-                break;
-            case Operation.X:
-                stackSize -= 1;
-                break;
-        }
+        stackSize += op === Operation.S ? +1 : -1;
         if (stackSize < 0) {
             return false;
         }
