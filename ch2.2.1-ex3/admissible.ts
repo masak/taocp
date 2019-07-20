408c6390556917eb5525d8bf892a2ad351cd26fb
Carl Mäsak <cmasak@gmail.com>
2019-07-20 21:22:53 +0200

    Turn `for` into `.every`

--- ch2.2.1-ex3/admissible.ts
+++ ch2.2.1-ex3/admissible.ts
@@ -2,12 +2,9 @@ import { Operation } from "../ch2.2.1-ex2/operations";
 
 export function sequenceIsAdmissible(sequence: Operation[]): boolean {
     let stackSize = 0;
-
-    for (let op of sequence) {
-        if ((stackSize += op === Operation.S ? +1 : -1) < 0) {
-            return false;
-        }
-    }
-
-    return !stackSize;
+    return (
+        sequence.every(
+            (op): boolean => (stackSize += op === Operation.S ? +1 : -1) >= 0,
+        ) && !stackSize
+    );
 }
