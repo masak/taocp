60af8effdb299999d8cdf68173c79ea6a4b20739
Carl Mäsak <cmasak@gmail.com>
2019-07-20 20:57:06 +0200

    Inline call to `opsToPerm`

--- ch2.2.1-ex3/admissible.ts
+++ ch2.2.1-ex3/admissible.ts
@@ -1,9 +1,35 @@
 import { Operation } from "../ch2.2.1-ex2/operations";
-import { opsToPerm } from "../ch2.2.1-ex2/operations-to-permutation";
+import {
+    CannotPopEmptyStack,
+    CarsLeftOnStack,
+} from "../ch2.2.1-ex2/operations-to-permutation";
 
 export function sequenceIsAdmissible(sequence: Operation[]): boolean {
     try {
-        opsToPerm(sequence);
+        let nextCarNumber = 1;
+        let permutation: number[] = [];
+        let stack: number[] = [];
+
+        for (let op of sequence) {
+            switch (op) {
+                case Operation.S:
+                    stack.push(nextCarNumber);
+                    nextCarNumber++;
+                    break;
+                case Operation.X:
+                    let number = stack.pop();
+                    if (typeof number === "undefined") {
+                        throw new CannotPopEmptyStack();
+                    }
+                    permutation.push(number);
+                    break;
+            }
+        }
+
+        if (stack.length) {
+            throw new CarsLeftOnStack();
+        }
+
         return true;
     } catch {
         return false;
